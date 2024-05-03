4fa8422ca072b3dff0b7d08b17c8b9b6a19df372
theotron0 <144718311+theotron0@users.noreply.github.com>
2024-05-03 19:36:25 +0000

    javascript

---
+++ scripts/main.js
@@ -0,0 +1,67 @@
+ //task 1
+console.log("task 1")
+for (i=1; i <11; i++){
+    console.log(i*i);
+}
+//task 2
+
+
+console.log("task 2")
+    for(i=5; i >= 0; i--){
+    console.log(i);
+}
+console.log("blastoff");
+//task 3
+ console.log("task 3")
+    for(i=0; i <= 50; i+= 2){
+        console.log(i)
+    }
+//task 4
+
+
+console.log("task 4")
+let sum = 0
+    for(let i=0; i <= 100; i++){
+        sum += i
+        console.log(sum)
+    }
+
+
+//task 5
+console.log("task 5")
+    let factorial = 1;
+    for(let i = 1; i<=10; i++){
+        factorial *= i;
+        console.log(factorial)
+    }
+
+
+//task 6
+console.log("task 6")
+let arr = [3, 1, 4, 1, 5];
+for (i = 0; i < arr.length; i++)
+console.log(arr[i]);
+ //task 7
+console.log("task 7")
+ for (let i = 0; i < arr.length; i++)
+ console.log(arr.at(-i-1));
+//task 8
+console.log("task 8")
+let cubenumbers = []
+for(let i = 0; i <= 10; i++)
+console.log(i**3)
+//task 9
+console.log("task 9")
+let arrFib = [0,1];
+for (let i = 0; i < 10; i++) {
+    arrFib.push(arrFib.at(-1)+arrFib.at(-2))
+}
+console.log(arrFib)
+//task 10
+console.log("task 10")
+let reverse = [];
+for (let i = 0; i < arr.length; i++) {
+    reverse.push(arr.at(-i - 1));
+}
+console.log(reverse);
+ 
